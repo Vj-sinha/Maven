5ed27d956496a2ebfdf07cb21683751180d59731
Dell <vs9054844@gmail.com>
2020-07-07 15:46:41 +0530

    Sample Maven

---
+++ first-maven-sample/src/main/java/com/vijay_july6/Sample1.java
@@ -0,0 +1,9 @@
+package com.vijay_july6;
+
+public class Sample1 {
+	public String returnString()
+	{
+		return "";
+	}
+
+}
---
+++ first-maven-sample/src/test/java/com/vijay_july6/Sample1Test.java
@@ -0,0 +1,14 @@
+package com.vijay_july6;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Sample1Test {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+	}
+
+}
